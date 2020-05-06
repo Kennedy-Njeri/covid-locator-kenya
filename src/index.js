44afd0c61629980e0f013d8e75bf0cfef3bfe6e7
Kennedy Njeri <mistakenz123@gmail.com>
2020-05-06 18:35:01 +0300

    Project setup

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+require('./db/mongoose')
+const covidRouter = require('./routes/covid')
+
+
+
+
+dotenv.config({ path: './src/config/config.env'})
+
+//console.log(require('dotenv').config({ path: './src/config/config.env'}))
+
+const app = express()
+
+// body parser
+app.use(express.json())
+
+// cors
+app.use(cors())
+
+
+app.use(covidRouter)
+
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
+
---
+++ src/routes/covid.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = new express.Router()
+
+
+
+
+
+router.get('/covid', () => {
+    console.log('covid')
+})
+
+
+
+
+
+
+module.exports = router
